9ffa8de8ee7918ce12ae983856534472776f17c2
Alan Bernstein <alan.bernstein@pm.me>
2021-06-20 11:02:57 +0200

    fix image issue, add css parts

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,9 @@
-(function () {
-    const storage = localStorage.getItem('data-theme');
-    if (storage) {
-        document.documentElement.setAttribute('data-theme', storage)
-        return;
-    }
-
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.documentElement.setAttribute('data-theme', 'dark')
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light')
-    }
-})();
-
 const themeChanger = document.querySelector('.changer');
 themeChanger.addEventListener('click', event => {
     event.preventDefault();
 
     let theme = document.documentElement
         .getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-theme', theme)
+    document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('data-theme', theme);
-
-    let img = document.querySelector('#gh-logo');
-    img.src = theme === 'dark' ? 'media/GitHub-light.png' : 'media/GitHub-dark.png';
-})
+});
